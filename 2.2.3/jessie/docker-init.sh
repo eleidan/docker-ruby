56410b43102bddbd205d79437f321ed80c9b0a12
Oleg Kulyk <helh.saintman@gmail.com>
2017-04-29 23:16:11 +0300

    Project clean up

--- 2.2.3/jessie/docker-init.sh
+++ 2.2.3/jessie/docker-init.sh
@@ -9,13 +9,6 @@ echo "Copying files from $RBENV_DIR (container) to $RUBY_DIR (host) ..." \
   && cp -r $RBENV_DIR/* $RUBY_DIR \
   && echo "Done."
 
-SCRIPTS_DIR="$DOCKER_DIR/scripts"
-CONTAINER_SCRIPTS_DIR="$HOME/scripts"
-echo "Copying files from $CONTAINER_SCRIPTS_DIR (container) to $SCRIPTS_DIR (host) ..." \
-  && mkdir -p $SCRIPTS_DIR \
-  && cp -r $CONTAINER_SCRIPTS_DIR/* $SCRIPTS_DIR \
-  && echo "Done."
-
 DOCKER_FILES_DIR="$HOME/recipes"
 echo "Copying files from $DOCKER_FILES_DIR (container) to project directory (host) ..." \
   && cp -r $DOCKER_FILES_DIR/* . \
